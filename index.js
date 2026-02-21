7e00c2a2eba7a956c3b99da4123affe686dc591d
xamdp <64017084+xamdp@users.noreply.github.com>
2026-02-21 12:28:58 +0900

    Add button functionality

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
 
-let firstCard = 3
-let secondCard = 6
+let message = "";
 
-let sum = firstCard + secondCard
-console.log(sum)
+function startGame() {
+
+	if (sum <= 20) {
+		message = "Do you want to draw a new card?";
+	} else if (sum === 21) {
+		message = "Wohoo! You've got Blackjack!";
+		hasBlackJack = true;
+	} else {
+		message = "You're out of the game!";
+		isAlive = false;
+	}
+
+	console.log(message)
+}
+
