c18d58e70576bad223f0e6abc4d7655c74b57600
xamdp <64017084+xamdp@users.noreply.github.com>
2026-02-22 14:09:34 +0900

    Add getRandomNumber function, Fix startGame functionality

--- index.js
+++ index.js
@@ -5,20 +5,41 @@ let cardsEl = document.querySelector('#cards-el')
 
 let message = "";
 let hasBlackJack = false;
-let isAlive = true;
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard;
+let isAlive = false
 
-function newCard() {
-	console.log("Drawing new card from the deck.")
-	let card = 7
-	sum += card
-	startGame()
-}
+let cards = [] // cards array
+let sum = 0
+
+console.log(cards)
+
+function getRandomCard() {
+	let randomNumber = Math.floor(Math.random() * 13) + 1
+	if (randomNumber > 10) {
+		return 10
+	} else if (randomNumber === 1) {
+		return 11
+	}
+	return randomNumber
+} 
 
 function startGame() {
-	cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+	isAlive = true
+	let firstCard = getRandomCard()
+	let secondCard = getRandomCard()
+	cards.push(firstCard, secondCard)
+	// sumEl.textContent += "Cards: " + cards[0] + " " + cards[1]
+	sum = firstCard + secondCard
+	console.log(cards)
+
+	renderGame()
+}
+
+function renderGame() {
+	cardsEl.textContent = "Cards: "
+	for (let card = 0; card < cards.length; card++){
+		cardsEl.textContent += cards[card] + " ";
+	}
+
 	sumEl.textContent = "Sum: " + sum
 	if (sum <= 20) {
 		message = "Do you want to draw a new card?";
@@ -31,7 +52,15 @@ function startGame() {
 	}
 
 	messageEl.textContent = message
-	console.log(sum)
 }
+
+function newCard() {
+	let card = getRandomCard()
+	console.log(getRandomCard())
+	sum += card
+	cards.push(card)
+	renderGame()
+}
+
 
 
